87cdcb17f283e80219f842d8ec8470a9d4b4d08a
Max Plomer <maxplomer@gmail.com>
2015-10-28 22:14:53 -0400

    calculate filenames

--- get_cat_data.m
+++ get_cat_data.m
@@ -4,12 +4,29 @@ function [ picture_array, xmin, xmax, ymin, ymax ] = get_cat_data()
     str = strrep(str, '"', '''');
     str = strrep(str, '[', '{');
     str = strrep(str, ']', '}');
-    picture_array = eval(str);
+    cat_listing_array = eval(str);
+    
+    picture_array = cell(9997,1);
+    xmin = cell(9997,1);
+    xmax = cell(9997,1);
+    ymin = cell(9997,1);
+    ymax = cell(9997,1);
+    
+    for i=1:length(cat_listing_array)
+        filename = strcat(cat_listing_array(i), '.jpg');
+        filename = strcat('cat_dataset/', filename);
+        picture_array{i} = filename;  
+    end
+  
     
     
     
     
     
-
-
+    
+    
+    
+    
+    
+    
 end
